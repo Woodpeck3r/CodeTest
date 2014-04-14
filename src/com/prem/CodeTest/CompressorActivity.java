e922cb982b162034c3a546ad01d0204b323a7956
Prembabu Panchadara <prem@eggheadcreative.com>
2014-04-14 19:32:08 +0530

    Implemented Basic onClick event to CompressorButton

--- src/com/prem/CodeTest/CompressorActivity.java
+++ src/com/prem/CodeTest/CompressorActivity.java
@@ -2,10 +2,12 @@ package com.prem.CodeTest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.L;
 import android.widget.TextView;
+import org.json.JSONException;
 
 public class CompressorActivity extends Activity {
 
@@ -19,7 +21,7 @@ public class CompressorActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.compressor);
     getWidgets();
-
+    applyActionToButton();
   }
 
   private void getWidgets() {
@@ -29,4 +31,37 @@ public class CompressorActivity extends Activity {
   }
 
 
+  private void applyActionToButton() {
+
+    compressButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String inputString = inputText.getText().toString();
+        if (inputString == null || inputString.length() == 0) {
+          inputText.setError("Please Enter Input");
+        } else {
+          String compressedString = compressText(inputString);
+          compressedText.setVisibility(View.VISIBLE);
+          compressedText.setText(compressedString);
+        }
+      }
+    });
+  }
+
+  private String compressText(String inputString) {
+
+    String[] words = inputString.split(" ");
+    String compressedString = new String(inputString);
+
+    for (String word : words) {
+      if (JSONHelper.jsonObject.has(word)) {
+        try {
+          compressedString = compressedString.replace(word, JSONHelper.jsonObject.getString(word));
+        } catch (JSONException e) {
+          Log.e("Json Exception:", e.getMessage());
+        }
+      }
+    }
+    return compressedString;
+  }
 }
